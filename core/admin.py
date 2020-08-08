4261437affbb73fe7a1e164f1cd2a9a8fba2fdf2
hyperleex <51726132+hyperleex@users.noreply.github.com>
2020-08-08 19:59:40 +0300

    admin + models + migrations

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from core.models import Serial, Genre, Series
+
+
+@admin.register(Serial)
+class SerialAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_featured', 'is_buy_forever')
+
+
+admin.site.register(Genre)
+admin.site.register(Series)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Serial(models.Model):
+    name = models.CharField(max_length=255)
+    short_description = models.CharField(max_length=512)
+    description = models.TextField()
+
+    genres = models.ManyToManyField(Genre, related_name='serials')
+
+    is_buy_forever = models.BooleanField(default=False)
+    is_featured = models.BooleanField(default=False)
+    poster = models.ImageField(upload_to='serials')
+
+    release_date = models.DateTimeField(
+        null=True, blank=True, verbose_name='Дата выхода'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Series(models.Model):
+    name = models.CharField(max_length=255)
+    serial = models.ForeignKey(Serial, related_name='series', on_delete=models.CASCADE)
+    description = models.TextField()
+    poster = models.ImageField(upload_to='series')
+    release_date = models.DateTimeField(
+        null=True, blank=True, verbose_name='Дата выхода'
+    )
+
+    def __str__(self):
+        return self.name
