f1c17a867a665b1b5981b89fdf6c902f3021b611
hyperleex <51726132+hyperleex@users.noreply.github.com>
2020-08-09 11:48:38 +0300

    admin + models + migrations

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from core.models import Serial, ShowCase
+
+
+def home_view(request):
+    serials = Serial.objects.filter(is_featured=True)
+    ctx = {
+        'serials': serials,
+        'showcases': ShowCase.objects.all().prefetch_related('serials')
+    }
+    return render(request, 'core/home.html', context=ctx)
+
+
+def catalog_view(request):
+    return render(request, 'core/catalog.html')
